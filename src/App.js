7ab067644c18303a16cfbf0bf109eaa45bc9f22d
NikolaPopovic71 <nikolapopovic71@gmail.com>
2024-10-31 17:21:35 +0100

    Added multiple Counter components with different titles in App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <h1>Counter App</h1>
-      <Counter title="My Counter" />
+      <Counter title="Counter 1" />
+      <Counter title="Counter 2" />
     </div>
   );
 }
+
 
 
 export default App;
